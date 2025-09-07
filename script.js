b1f108127fe9b1153e7644d1a9140aa130453a9e
ABHI786-tech <abhiofficial453@gmail.com>
2025-09-07 13:39:47 +0530

    feat:added animation or js

---
+++ script.js
@@ -0,0 +1,78 @@
+// get the fixed navbar height
+let navbar = document.querySelector(".navbar-nav");
+let navbarHeight = navbar ? navbar.offsetHeight : 0;
+
+let sections = document.querySelectorAll(".container-lg"); // target all sections
+let navlink = document.querySelectorAll(".nav-link");
+
+
+window.onscroll = () => {
+    sections.forEach(se => {
+        let top = window.scrollY;
+        let offset = se.offsetTop - navbarHeight; // start detecting before section
+        let height = se.offsetHeight + navbarHeight; // include navbar height
+        let id = se.getAttribute("id");
+
+        if (top >= offset && top < offset + height) {
+            navlink.forEach(links => {
+                links.classList.remove("active");
+            });
+
+            // select nav link that matches the section id
+            let currentLink = document.querySelector(`.nav-link[href="#${id}"]`);
+            if (currentLink) {
+                currentLink.classList.add("active", true);
+            }
+        }
+    });
+
+    const navbarLinks = document.querySelectorAll('.navbar-nav .nav-link');
+
+navbarLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    // pehle sabhi links se class hata do
+    navbarLinks.forEach(l => l.classList.remove('active-link'));
+    // jis link par click hua usme class add karo
+    link.classList.add('active-link');
+  });
+});
+
+
+
+
+    // close the menu when a link is clicked (for mobile view)
+    let navLinks = document.querySelectorAll('.nav-link');
+    let navbarCollapse = document.querySelector('.navbar-collapse');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            let bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+                toggle: false
+            });
+            bsCollapse.hide(); // close the menu
+        });
+    });
+};
+
+
+// scroll reveal  animation
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+ScrollReveal().reveal('.homeContent, .heading', { origin: 'top' });
+ScrollReveal().reveal('img', { origin: 'bottom' });
+ScrollReveal().reveal('form, .card', { origin: 'bottom' });
+ScrollReveal().reveal('.homeContent h1, .aboutImg', { origin: 'left' });
+ScrollReveal().reveal('.homeContent p, .Content', { origin: 'right' });
+
+
+const multi = new Typed('.multi-text', {
+    strings: ['Frontend Developer', 'Web Developer', 'Blogger'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
